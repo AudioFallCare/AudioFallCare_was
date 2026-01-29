157c70744ba89410c3d48aa7c14df073714ba4f5
ming920 <rkfsky920@gmail.com>
2026-01-29 14:20:21 +0900

    build: 프로젝트 생성

---
+++ src/main/java/com/bumil/audio_fall_care/AudioFallCareApplication.java
@@ -0,0 +1,13 @@
+package com.bumil.audio_fall_care;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudioFallCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudioFallCareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumil/audio_fall_care/AudioFallCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumil.audio_fall_care;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioFallCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
